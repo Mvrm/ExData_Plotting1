a799dc2076e233a93170cce3cd34a896df5ed7f6
Manish <manishvrmv@gmail.com>
2014-05-11 17:29:21 +0530

    plot

--- plot2.R
+++ plot2.R
@@ -1,11 +1,11 @@
 house_data <- read.table('household_power_consumption.txt', na.strings='?', sep=';', header=T, colClasses = c('character', 'character', 'numeric','numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'))
 
-house_data$DateTime <- strptime(paste(house_data$Date, house_data$Time), "%d/%m/%Y %H:%M:%S")
+house_data$DateTime  <- strptime(paste(house_data$Date, house_data$Time), "%d/%m/%Y %H:%M:%S")
 
 house_data <- subset(house_data, as.Date(DateTime) >= as.Date("2007-02-01") & as.Date(DateTime) <= as.Date("2007-02-02"))
 
-png("plot1.png", height=480, width=480, units = "px")
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
 
-hist(house_data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+plot(house_data$DateTime, house_data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
 dev.off()
